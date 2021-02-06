fdea61293da000c4a68c2359df456bc06703ffe6
Tahoe <50768582+Tahoooe@users.noreply.github.com>
2021-02-06 21:45:34 +0100

    End of overall + list in the selected order

--- includes/scripts.js
+++ includes/scripts.js
@@ -3,16 +3,7 @@ cl = name => document.getElementsByClassName(name);
 
 const list = document.getElementById('items');
 
-
-// retourne la place d'un li
-function getIndex(node) {
-  let children = node.parentNode.children;
-
-  for (i = 0; i < children.length; i++) {
-    if (node == children[i]) break;
-  }
-  return i + 1;
-}
+function resetList() { list.innerHTML = ''; }
 
 // accordéon volé
 const accordionBtns = document.querySelectorAll('.accordion');
@@ -31,185 +22,134 @@ accordionBtns.forEach((accordion) => {
   };
 });
 
+// retourne les slugs de la liste
+function getValues(origin) {
+    let elements;
+    let names = [];
 
-
-
-// compteur de races
-function countChecked() {
-  let checkedBoxes = document.querySelectorAll('input[type="checkbox"]:checked');
-
-  // compte les valeurs et retire les dupliquées
-  let values = [];
-  checkedBoxes.forEach((checkedBox) => {
-    values.push(checkedBox.value);
-  });
-
-  values = [...new Set(values)];
-  
-  // ajoute dans le compteur
-  if (!values) {
-    id('compteur').innerHTML = 'No breed selected.';
-  } else if (values.length === 1) {
-    id('compteur').innerHTML = values.length + ' breed selected.';
-  } else {
-    id('compteur').innerHTML = values.length + ' breeds selected.';
-  }
+    if (origin === "checkboxes") {
+      elements = document.querySelectorAll('input[type="checkbox"]:checked');
+      elements.forEach((element) => {
+        names.push(element.value.toString());
+      })
+    } else if (origin === "list") {
+      elements = document.querySelectorAll('#items > li');
+      elements.forEach((element) => {
+        names.push(element.classList.toString());
+      })
+    }
+    
+    names = [...new Set(names)];
+    
+    return names;
 }
+
+
+
+// retourne la place d'un li
+function getIndex(node) {
+    let children = node.parentNode.children;
+  
+    for (i = 0; i < children.length; i++) {
+      if (node == children[i]) break;
+    }
+    return i + 1;
+  }
+
+  
+// donne leur place aux éléments de la liste
+function listIndexes() {
+    let elements = document.querySelectorAll('#items > li');
+
+    // pour chaque li, trouver sa place
+    elements.forEach((element) => {
+        let place = element.querySelector('.place');
+        place.innerHTML = '#' + getIndex(element);
+    })
+}
+
+
+
+
+// mise à jour de la liste
+// c'est sûrement pas ouf mais je vois pas comment faire autrement
+function sendToList(breedSlug) {
+    let breedData = document.querySelector('.' + breedSlug).children;
+    let breedName = breedData[2].innerHTML;
+    let breedImage = breedData[0].currentSrc;
+
+    let fullLi = '<li class="' + breedSlug + '"><div class="delete"><img src="http://localhost/~tahoe/myfavoritedogs/images/moins.svg"></div><div class="breed"><img class="breedImage" src="' + breedImage + '"><span><span class="place"></span> - ' + breedName + '</span><img class="dragIcon" src="http://localhost/~tahoe/myfavoritedogs/images/drag.svg"></div></li>'; 
+
+    list.insertAdjacentHTML('beforeend', fullLi);
+}
+
 
 
 // Actions quand les checkbox changent 
 let checkboxes = document.querySelectorAll('input[type="checkbox"]');
 checkboxes.forEach((checkbox) => {
 
-  function checkToo() {
-    
-    // trouve toutes les checkbox qui ont la même valeur que celle cliquée
-    let toCheck = document.querySelectorAll('input[value="' + checkbox.value + '"');
-    
-    toCheck.forEach((value) => {
-      // clique sur les checkbox qui ne sont pas celle cliquée
-      if (value != checkbox) {
-        value.click();
-      }
-    });
-  }
+    // coche les checkbox identiques
+    checkbox.addEventListener("change", checkToo);
+    function checkToo() {
+        // trouve toutes les checkbox qui ont la même valeur que celle cliquée
+        let toCheck = document.querySelectorAll('input[value="' + checkbox.value + '"');
 
-  // coche les checkbox identiques
-  checkbox.addEventListener("change", checkToo);
-
-  // compte les checkbox cochées
-  checkbox.addEventListener("change", countChecked);
-
-  // Ajout dans le local storage + update de la liste
-  // checkbox.addEventListener("change", sendToStorage); 
-
-  checkbox.addEventListener("change", maybeSendToList);
-
-  function maybeSendToList() {
-    if (checkbox.checked) {
-      
-      // ajoute si la liste est vide
-      if (getListValues().length === 0) {
-        sendToList(checkbox.value);
-      } 
-
-
-      
-    } else {
-      getListValues().forEach((listValue) => {
-        
-        if (listValue === checkbox.value) {
-          console.log(document.querySelector('#items li.' + listValue));
-          document.querySelector('#items li.' + listValue).remove();
-        }
-        
-      })
-      
+        toCheck.forEach((value) => {
+            // clique sur les checkbox qui ne sont pas celle cliquée
+            if (value != checkbox) {
+            value.click();
+            }
+        });
     }
-  }
-  
+
+    // met à jour la liste
+    checkbox.addEventListener("change", updateList);
 });
 
-function getListValues() {
-  let elements = document.querySelectorAll('#items > li');
-  let names = [];
-  elements.forEach((element) => {
-    names.push(element.classList.toString());
-  })
 
-  names = [...new Set(names)];
-  
-  return names;
-}
+function updateList() {
+    resetList();
 
+    getValues("checkboxes").forEach((value) => {
+        sendToList(value);
+    })
 
-// mise à jour de la liste
-// c'est sûrement pas ouf mais je vois pas comment faire autrement
-function sendToList(breedSlug) {
-
-  let breedData = document.querySelector('.' + breedSlug).children;
-  let breedName = breedData[2].innerHTML;
-  let breedImage = breedData[0].currentSrc;
-
-  let fullLi = '<li class="' + breedSlug + '"><div class="delete"><img src="http://localhost/~tahoe/myfavoritedogs/images/moins.svg"></div><div class="breed"><img class="breedImage" src="' + breedImage + '"><span><span class="place"></span> - ' + breedName + '</span><img class="dragIcon" src="http://localhost/~tahoe/myfavoritedogs/images/drag.svg"></div></li>'; 
-
-  list.insertAdjacentHTML('beforeend', fullLi);
-  listIndexes();
-  
+    listIndexes();
+    counter();
+    sendToStorage();
 }
 
 
 
-// function sendToStorage() {
-
-//   // reset le storage
-//   localStorage.clear();
-
-//   // trouve toutes les checkbox cochées
-//   let checkedBoxes = document.querySelectorAll('input[type="checkbox"]:checked');
-  
-//   // ajoute toutes les checkbox cochées dans l'array values
-//   let values = [];
-//   checkedBoxes.forEach((checkedBox) => {
-//     values.push(checkedBox.value);
-//   });
-
-//   // retire les doublons
-//   values = [...new Set(values)];
-  
-
-//   // envoie dans le storage
-//   localStorage.setItem("breeds", JSON.stringify(values));
-//   console.log(values + "envoyés dans le storage")
-//   // update la liste
-//   // list.innerHTML = '';
-//   // values.forEach((value) => {
-//   //   sendToList(value);
-    
-//   // })
-
-// }
-
-
-
-// donne leur place aux éléments de la liste
-function listIndexes() {
-  let elements = document.querySelectorAll('#items > li');
-
-  // pour chaque li, trouver sa place
-  elements.forEach((element) => {
-    let place = element.querySelector('.place');
-    place.innerHTML = '#' + getIndex(element);
-  })
+//compteur de races
+function counter() {
+  // ajoute dans le compteur
+  if (getValues("checkboxes").length === 0) {
+    id('compteur').innerHTML = 'No breed selected.';
+  } else if (getValues("checkboxes").length === 1) {
+    id('compteur').innerHTML = getValues("checkboxes").length + ' breed selected.';
+  } else {
+    id('compteur').innerHTML = getValues("checkboxes").length + ' breeds selected.';
+  }
 }
 
 
-
-
-
-
-
-
-
-
+function sendToStorage() {
+    // reset le storage
+    localStorage.clear();
+    localStorage.setItem("breeds", JSON.stringify(getValues("list")));
+}
 
 
 // liste drag and sort
 let sortable = Sortable.create(list, {
-  animation: 150,
-  onEnd: function() {
-    listIndexes();
-  }
+    animation: 150,
+    onEnd: function() {
+        listIndexes();
+        sendToStorage();
+    }
 });
-
-
-
-
-
-
-
-
-
 
 
 
@@ -224,30 +164,14 @@ window.onload = function() {
         checkboxes.forEach((checkbox) => {
           if (checkbox.value === value) {
             checkbox.checked = true;
-            countChecked();
           }
         });
         sendToList(value);
-
+        counter();
+        listIndexes();
       })
   }
   
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  
+  
