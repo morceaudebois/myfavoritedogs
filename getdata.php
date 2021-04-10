ba2f2a2736596867a8e7bee76c8ecf4639d21dff
Tahoe <50768582+Tahoooe@users.noreply.github.com>
2021-04-10 22:21:46 +0200

    JS list overall

--- getdata.php
+++ getdata.php
@@ -1,17 +1,28 @@
 <?php require_once('includes/config.php');
 
+
 $slug = $_GET['q'];
+
 
 try {
     // Va chercher les URL des photos
-    $stmt = $db->prepare('SELECT photo_url FROM photos INNER JOIN dogbreeds ON dogbreeds.id = photos.breed_id WHERE dogbreeds.slug = :slug');
+    $stmt1 = $db->prepare('SELECT photo_url FROM photos INNER JOIN dogbreeds ON dogbreeds.id = photos.breed_id WHERE dogbreeds.slug = :slug');
+    $stmt1->execute(array( ':slug' => $slug ));
+    $breed = $stmt1->fetch();
 
-    $stmt->execute(array( ':slug' => $slug ));
-    
-    $breed = $stmt->fetch();
 
-    echo $breed['photo_url'];
-    // var_dump($breed);
+
+    $stmt2 = $db->prepare('SELECT * FROM dogbreeds WHERE dogbreeds.slug = :slug');
+    $stmt2->execute(array( ':slug' => $slug ));
+    $text = $stmt2->fetch();
+
+    $fullData = array(
+        'name' => $text['title'],
+        'slug' => $text['slug'],
+        'photo_url' => $breed['photo_url']
+    );
+
+    echo json_encode($fullData);
 
 } catch(PDOException $e) {
     echo $e->getMessage();
--- includes/scripts.js
+++ includes/scripts.js
@@ -1,292 +1,225 @@
 id = name => document.getElementById(name);
 cl = name => document.getElementsByClassName(name);
-const baseurl = window.location.origin+window.location.pathname;
+all = name => document.querySelectorAll(name);
+const baseURL = window.location.origin+window.location.pathname;
+const list = id('items');
 
-(function() {
-  // your page initialization code here
-  // the DOM will be available here
-  window.addEventListener('resize', addScreenWidthClasses);
+// retourne la place d'un li
+function getIndex(node) {
+  let children = node.parentNode.children;
 
-  function addScreenWidthClasses() {
-      if (window.innerWidth < 489) {
-          document.body.classList.remove('mobile', 'smallTablet', 'tablet', 'desktop');
-          document.body.classList.add('mobile');
-      } else if (window.innerWidth < 768)  {
-          document.body.classList.remove('mobile', 'smallTablet', 'tablet', 'desktop');
-          document.body.classList.add('smallTablet');
-      } else if (window.innerWidth < 992) {
-          document.body.classList.remove('mobile', 'smallTablet', 'tablet', 'desktop');
-          document.body.classList.add('tablet');
-      } else {
-          document.body.classList.remove('mobile', 'smallTablet', 'tablet', 'desktop');
-          document.body.classList.add('desktop');
-      }
+  for (i = 0; i < children.length; i++) {
+    if (node == children[i]) break;
   }
-
-  addScreenWidthClasses();
-})();
-
-const list = document.getElementById('items');
-
-function resetList() {
-  list.innerHTML = '';
-
-  // reset l'enregistrement de liste
-  cl('nameContainer')[0].style.display = "flex";
-  cl('linkContainer')[0].style.display = "none";
-  id('copyLink').innerHTML = 'Copy link';
-  id('share').style.display = "none";
+  return i + 1;
 }
 
-// accordéon volé
-const accordionBtns = document.querySelectorAll('.accordion');
-let index = 0;
-accordionBtns.forEach((accordion) => {
+// clique sur une race 
+function check(breed) {
+  document.querySelector('input[value="' + breed + '"').click();
+}
+
+function addScreenWidthClasses() {
+  if (window.innerWidth < 489) {
+      document.body.classList.remove('mobile', 'smallTablet', 'tablet', 'desktop');
+      document.body.classList.add('mobile');
+  } else if (window.innerWidth < 768)  {
+      document.body.classList.remove('mobile', 'smallTablet', 'tablet', 'desktop');
+      document.body.classList.add('smallTablet');
+  } else if (window.innerWidth < 992) {
+      document.body.classList.remove('mobile', 'smallTablet', 'tablet', 'desktop');
+      document.body.classList.add('tablet');
+  } else {
+      document.body.classList.remove('mobile', 'smallTablet', 'tablet', 'desktop');
+      document.body.classList.add('desktop');
+  }
+}
+window.addEventListener('resize', addScreenWidthClasses);
+
+window.onload = function() {
+  let values = JSON.parse(localStorage.getItem("breeds"));
+
+  // s'il y a quelque chose dans le storage, cocher + mettre dans la liste
+  if (values.length) {
+    values.forEach((value) => { check(value) })
+  }
+
+  // accordéon volé
+  const accordionBtns = document.querySelectorAll('.accordion');
+  let index = 0;
+  accordionBtns.forEach((accordion) => {
+
+    accordion.onclick = function () { 
+      this.classList.toggle('isOpen');
+      let content = this.nextElementSibling;
+      if (content.style.maxHeight) {
+        //this is if the accordion is open
+        content.style.maxHeight = null;
+      } else {
+        //if the accordion is currently closed
+        content.style.maxHeight = content.scrollHeight + 'px';
+      }
+    };
+
+    // ouvre le premier au chargement 
+    if (index === 0) { accordion.click(); } index++;
+
+    // fix le responsive
+    document.getElementsByTagName('body')[0].onresize = function() {
+      let areOpen = document.querySelectorAll('.isOpen');
+      
+      areOpen.forEach((isOpen) => {
+          let content = isOpen.nextElementSibling;
+          content.style.maxHeight = content.scrollHeight + 'px'; 
+      })
+    };
+
+  });
+
+  addScreenWidthClasses();
+};
+
+
+(function() {
+  // when inputs get clicked on
+  all('input[type="checkbox"]').forEach((checkbox) => {
+    checkbox.onchange = function() {
+
+      // checks or unchecks everything at once
+      let toChecks = document.querySelectorAll('input[value="' + checkbox.value + '"');
+      toChecks.forEach((toCheck) => { toCheck.checked = this.checked; });
+
+      updatePannel(checkbox.value);
+
+    };
+  });
+})();
+
+
+function updatePannel(breedSlug) {
   
-  accordion.onclick = function () { 
-    this.classList.toggle('isOpen');
-    let content = this.nextElementSibling;
-    if (content.style.maxHeight) {
-      //this is if the accordion is open
-      content.style.maxHeight = null;
-    } else {
-      //if the accordion is currently closed
-      content.style.maxHeight = content.scrollHeight + 'px';
-    }
-  };
+  // retire le li déjà existant si décoché
+  if (document.querySelector('.list ul li.' + breedSlug)) {
+    document.querySelector('.list ul li.' + breedSlug).remove();
+    listIndexes(); sendToStorage(); counter();
+  // va chercher les infos dans la bdd et ajoute le li
+  } else {
 
-  // ouvre le premier au chargement 
-  if (index === 0) { accordion.click(); } index++;
-
+    getData().then(function (response) {
+      let breedData = JSON.parse(response);
   
+      let fullLi = '<li class="' + breedData['slug'] + '"><div class="delete" onclick="check(\'' +  breedData['slug'] + '\')"><img draggable="false" src="' + baseURL + 'images/moins.svg"></div><div class="breed"><img draggable="false" class="breedImage" src="' + baseURL + 'images/smaller/' + breedData['photo_url'] + '"><span><span class="place"></span> - ' + breedData['name'] + '</span><img draggable="false" class="dragIcon" src="' + baseURL + 'images/drag.svg"></div></li>'; 
+      list.insertAdjacentHTML('beforeend', fullLi);
+  
+      listIndexes(); sendToStorage(); counter();
+  
+    }).catch(function (req) {
+      console.log('Can\'t get data: ' + req);
+    });
+  }
 
-  // fix le responsive
-  document.getElementsByTagName('body')[0].onresize = function() {
-    let areOpen = document.querySelectorAll('.isOpen');
-    
-    areOpen.forEach((isOpen) => {
-        let content = isOpen.nextElementSibling;
-        content.style.maxHeight = content.scrollHeight + 'px'; 
-    })
-  };
+  function getData() {
+    return new Promise(function (resolve, reject) {
 
+      let xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+          // resolve((baseURL + 'images/smaller/' + this.responseText).replace(/\s/g, ''));
+          resolve(this.responseText);
+        }
+      };
+
+      xhttp.open("GET", "getdata.php?q=" + breedSlug, true);
+      xhttp.send();
+    });
+  }
+}
+
+//compteur de races & pannel behaviour
+function counter() {
+  let elements = all('#items > li');
+
+  // ajoute dans le compteur
+  if (elements.length === 0) {
+    document.body.classList.remove('open');
+    document.body.classList.add('noSelection');
+  } else if (elements.length === 1) {
+    id('compteur').innerHTML = elements.length + ' breed selected.';
+    document.body.classList.remove('noSelection');
+  } else {
+    id('compteur').innerHTML = elements.length + ' breeds selected.';
+    document.body.classList.remove('noSelection');
+  }
+}
+
+// donne leur place aux éléments de la liste
+function listIndexes() {
+  // pour chaque li, trouver sa place
+  all('#items > li').forEach((element) => {
+      let place = element.querySelector('.place');
+      place.innerHTML = '#' + getIndex(element);
+  })
+}
+
+// liste drag and sort
+let sortable = Sortable.create(list, {
+  animation: 150,
+  onEnd: function() {
+      listIndexes();
+      sendToStorage();
+
+      cl('nameContainer')[0].style.display = "flex";
+      cl('linkContainer')[0].style.display = "none";
+      id('copyLink').innerHTML = 'Copy link';
+      id('share').style.display = "none";
+  }
 });
 
-
-
 // retourne les slugs de la liste
-function getValues(origin) {
-    let elements;
+function getFullList() {
     let names = [];
 
-    if (origin === "checkboxes") {
-      elements = document.querySelectorAll('input[type="checkbox"]:checked');
-      elements.forEach((element) => {
-        names.push(element.value.toString());
-      })
-    } else if (origin === "list") {
-      elements = document.querySelectorAll('#items > li');
-      elements.forEach((element) => {
-        names.push(element.classList.toString());
-      })
-    }
+    all('#items > li').forEach((element) => {
+      names.push(element.classList.toString());
+    })
     
     names = [...new Set(names)];
     
     return names;
 }
 
-
-
-// retourne la place d'un li
-function getIndex(node) {
-    let children = node.parentNode.children;
-  
-    for (i = 0; i < children.length; i++) {
-      if (node == children[i]) break;
-    }
-    return i + 1;
-  }
-
-  
-// donne leur place aux éléments de la liste
-function listIndexes() {
-    let elements = document.querySelectorAll('#items > li');
-
-    // pour chaque li, trouver sa place
-    elements.forEach((element) => {
-        let place = element.querySelector('.place');
-        place.innerHTML = '#' + getIndex(element);
-    })
-}
-
-
-
-
-
-// mise à jour de la liste
-// c'est sûrement pas ouf mais je vois pas comment faire autrement
-async function sendToList(breedSlug, justLoaded) {
-  let breedData = document.querySelector('.' + breedSlug).children;
-  let breedName = breedData[2].innerHTML;
-
-  function getImage() {
-    return new Promise(function (resolve, reject) {
-      if (!justLoaded) {
-        resolve(breedData[0].currentSrc.replace("small", "smaller"));
-      // goes get the thumbnail URL
-      } else {
-        let xhttp = new XMLHttpRequest();
-
-        xhttp.onreadystatechange = function() {
-          if (this.readyState == 4 && this.status == 200) {
-            resolve((baseurl + 'images/smaller/' + this.responseText).replace(/\s/g, ''));
-          }
-        };
-
-        xhttp.open("GET", "getdata.php?q=" + breedSlug, true);
-        xhttp.send();
-      }
-    });
-  }
-
-  getImage().then(function (response) {
-    let fullLi = '<li class="' + breedSlug + '"><div class="delete" onclick="del(this.parentNode)"><img draggable="false" src="' + baseurl + 'images/moins.svg"></div><div class="breed"><img draggable="false" class="breedImage" src="' + response + '"><span><span class="place"></span> - ' + breedName + '</span><img draggable="false" class="dragIcon" src="' + baseurl + 'images/drag.svg"></div></li>'; 
-    list.insertAdjacentHTML('beforeend', fullLi);
-
-  }).catch(function (req) {
-    console.log('Can\'t get data: ' + req);
-  });
-}
-
-
-
-// Actions quand les checkbox changent 
-let checkboxes = document.querySelectorAll('input[type="checkbox"]');
-checkboxes.forEach((checkbox) => {
-
-    // coche les checkbox identiques
-    checkbox.addEventListener("click", checkToo);
-    function checkToo() {
-        // trouve toutes les checkbox qui ont la même valeur que celle cliquée
-        let toCheck = document.querySelectorAll('input[value="' + checkbox.value + '"');
-
-        toCheck.forEach((value) => {
-            // clique sur les checkbox qui ne sont pas celle cliquée
-            
-            if (value != checkbox && (event.isTrusted)) {
-              value.click();
-            }
-        });
-    }
-
-    // met à jour la liste
-    checkbox.addEventListener("click", updateList);
-});
-
-
-function del(element) {
-  let toUnchecks = document.querySelectorAll('input[value="' + element.className + '"');
-
-  // décoche tout ce qu'il y a à décocher
-  toUnchecks.forEach((toUncheck) => {
-    toUncheck.click();
-  });
-
-}
-
-
-function updateList() {
-    resetList(); 
-
-    getValues("checkboxes").forEach((value) => {
-        sendToList(value, false);
-    })
-
-    listIndexes();
-    counter();
-    sendToStorage();
-}
-
-
-//compteur de races & pannel behaviour
-function counter() {
-  // ajoute dans le compteur
-  if (getValues("checkboxes").length === 0) {
-    id('compteur').innerHTML = 'No breed selected.';
-
-    document.body.classList.remove('open');
-    document.body.classList.add('noSelection');
-
-  } else if (getValues("checkboxes").length === 1) {
-    id('compteur').innerHTML = getValues("checkboxes").length + ' breed selected.';
-
-    document.body.classList.remove('noSelection');
-
-  } else {
-    id('compteur').innerHTML = getValues("checkboxes").length + ' breeds selected.';
-
-    document.body.classList.remove('noSelection');
-  }
-}
-
-
 function sendToStorage() {
-    // reset le storage
-    localStorage.clear();
-    localStorage.setItem("breeds", JSON.stringify(getValues("list")));
+  // reset le storage
+  localStorage.clear();
+  localStorage.setItem("breeds", JSON.stringify(getFullList()));
 }
 
 
-// liste drag and sort
-let sortable = Sortable.create(list, {
-    animation: 150,
-    onEnd: function() {
-        listIndexes();
-        sendToStorage();
 
-        cl('nameContainer')[0].style.display = "flex";
-        cl('linkContainer')[0].style.display = "none";
-        id('copyLink').innerHTML = 'Copy link';
-        id('share').style.display = "none";
-    }
+
+function pannel() {
+  document.body.classList.toggle('open');
+  id('pannel').scrollTop = 0; // For Safari
+}
+
+
+document.querySelector('.overlay').addEventListener('click', () => {
+  pannel();
 });
 
 
 
-
-window.onload = function() {
-  
-  let values = JSON.parse(localStorage.getItem("breeds"));
-
-  // s'il y a quelque chose dans le storage, cocher + mettre dans la liste
-  if (values) {
-    
-    values.forEach((value) => {
-        // recoche les cases à partir du local storage
-        checkboxes.forEach((checkbox) => {
-          if (checkbox.value === value) {
-            checkbox.checked = true;
-
-            // disables lazy loading for selected breeds
-            checkbox.parentNode.previousSibling.previousSibling.loading = "eager";
-          }
-        });
-        
-        sendToList(value, true);
-        
-      })
-
-      counter();
-      listIndexes();
+// enter dans l'input
+id('name') .onkeyup = function(e) {
+  if(e.keyCode == 13){
+    genLink(e);
   }
-};
+}
 
-
-
-
-
-
+// clic sur le bouton de génération de lien
+id("genLink").addEventListener('click', (e) => {
+  genLink(e);
+});
 
 // gère la génération de lien
 function genLink(e) {
@@ -346,50 +279,20 @@ function genLink(e) {
   }
 }
 
-// enter dans l'input
-id('name') .onkeyup = function(e) {
-  if(e.keyCode == 13){
-    genLink(e);
-  }
-}
-
-// clic sur le bouton de génération de lien
-id("genLink").addEventListener('click', (e) => {
-  genLink(e);
-});
-
-
-
-
 function copyLink() {
-    /* Get the text field */
-    var copyText = id("link");
+  /* Get the text field */
+  var copyText = id("link");
 
-    /* Select the text field */
-    copyText.select();
-    copyText.setSelectionRange(0, 99999); /* For mobile devices */
-  
-    /* Copy the text inside the text field */
-    document.execCommand("copy");
-  
-    /* Alert the copied text */
-    id('copyLink').innerHTML = 'Link has been copied';
+  /* Select the text field */
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /* For mobile devices */
 
-    setTimeout(function() { id('copyLink').innerHTML = 'Copy link';
-    }, 5000);
+  /* Copy the text inside the text field */
+  document.execCommand("copy");
+
+  /* Alert the copied text */
+  id('copyLink').innerHTML = 'Link has been copied';
+
+  setTimeout(function() { id('copyLink').innerHTML = 'Copy link';
+  }, 5000);
 }
-
-
-function pannel(el) {
-  document.body.classList.toggle('open');
-
-  id('pannel').scrollTop = 0; // For Safari
-  id('pannel').scrollTop = 0;
-}
-
-
-document.querySelector('.overlay').addEventListener('click', () => {
-  pannel();
-});
-
-  
