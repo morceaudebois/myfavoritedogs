73f7842a246c6e85afa8df7acef362c25c2ae287
Tahoe <50768582+morceaudebois@users.noreply.github.com>
2023-11-07 19:20:22 +0100

    _it work_

--- pannel.php
+++ pannel.php
@@ -1,82 +1,82 @@
 <div id="pannel">
-        <div class="bottom">
-            <div id="compteur">No breed selected.</div>
-            <button onclick="pannel(this)" class="niceBtn">my list</button>
-        </div>
-
-        <div class="list">
-            <div class="bigTitle">
-                <h2>My list</h2> <div class="hr"></div>
-            </div>
-
-            <ul id="items"></ul>
-        </div>
-        
-        <div class="saveList">
-            <div class="bigTitle">
-                <h2>Save your list</h2> <div class="hr"></div>
-            </div>
-    
-
-            <div class="doubleContainer nameContainer">
-                <div class="half">
-                    <div class="fieldContainer">
-                        <label for="name">What's your name?</label>
-                        <input type="text" id="name" name="name" minlength="4" maxlength="20" size="10" placeholder="Michael" autocomplete="off">
-                    </div>
-                </div>
-
-                <div class="half">
-                    <button id="genLink" class="niceBtn">Generate link</button>
-                </div>
-            </div>
-
-
-            <div class="doubleContainer linkContainer">
-                <div class="half">
-                    <div class="fieldContainer">
-                        <label for="link">Here's your link!</label>
-                        <input type="text" id="link" name="link" autocomplete="off">
-                    </div>
-                </div>
-
-                <div class="half">
-                    <button id="copyLink" class="niceBtn" onclick='copyLink()'>Copy link</button>
-                </div>
-            </div>
-
-            <span id="error"></span>
-            
-
-
-            <div id="share">
-                <div class="bigTitle">
-                    <h2>Share your list</h2> <div class="hr"></div>
-                </div>
-
-                <div class="socialContainer">
-                    <a target="_blank" class="socialBlock" id="twitterBlock">
-                        <img draggable='false' src="<?php echo $homeURL . "/images/twitter.svg" ?>">
-                        <span>Twitter</span>
-                    </a>
-
-                    <a target="_blank" class="socialBlock" id="facebookBlock">
-                        <img draggable='false' src="<?php echo $homeURL . "/images/facebook.svg" ?>">
-                        <span>Facebook</span>
-                    </a>
-
-                    <a target="_blank" class="socialBlock" id="telegramBlock">
-                        <img draggable='false' src="<?php echo $homeURL . "/images/telegram.svg" ?>">
-                        <span>Telegram</span>
-                    </a>
-                </div>
-            </div>
-
-            
-
-            
-
-        </div>
-        
-        
+    <div class="bottom">
+        <div id="compteur">No breed selected.</div>
+        <button onclick="pannel(this)" class="niceBtn">my list</button>
     </div>
+
+    <div class="list">
+        <div class="bigTitle">
+            <h2>My list</h2> <div class="hr"></div>
+        </div>
+
+        <ul id="items"></ul>
+    </div>
+    
+    <div class="saveList">
+        <div class="bigTitle">
+            <h2>Save your list</h2> <div class="hr"></div>
+        </div>
+
+
+        <div class="doubleContainer nameContainer">
+            <div class="half">
+                <div class="fieldContainer">
+                    <label for="name">What's your name?</label>
+                    <input type="text" id="name" name="name" minlength="4" maxlength="20" size="10" placeholder="Michael" autocomplete="off">
+                </div>
+            </div>
+
+            <div class="half">
+                <button id="genLink" class="niceBtn">Generate link</button>
+            </div>
+        </div>
+
+
+        <div class="doubleContainer linkContainer">
+            <div class="half">
+                <div class="fieldContainer">
+                    <label for="link">Here's your link!</label>
+                    <input type="text" id="link" name="link" autocomplete="off">
+                </div>
+            </div>
+
+            <div class="half">
+                <button id="copyLink" class="niceBtn" onclick='copyLink()'>Copy link</button>
+            </div>
+        </div>
+
+        <span id="error"></span>
+        
+
+
+        <div id="share">
+            <div class="bigTitle">
+                <h2>Share your list</h2> <div class="hr"></div>
+            </div>
+
+            <div class="socialContainer">
+                <a target="_blank" class="socialBlock" id="twitterBlock">
+                    <img draggable='false' src="<?php echo $homeURL . "/images/twitter.svg" ?>">
+                    <span>Twitter</span>
+                </a>
+
+                <a target="_blank" class="socialBlock" id="facebookBlock">
+                    <img draggable='false' src="<?php echo $homeURL . "/images/facebook.svg" ?>">
+                    <span>Facebook</span>
+                </a>
+
+                <a target="_blank" class="socialBlock" id="telegramBlock">
+                    <img draggable='false' src="<?php echo $homeURL . "/images/telegram.svg" ?>">
+                    <span>Telegram</span>
+                </a>
+            </div>
+        </div>
+
+        
+
+        
+
+    </div>
+    
+    
+</div>
--- test_sqlite.php
+++ test_sqlite.php
@@ -1,10 +1,13 @@
 <?php
+
+$slug = $_GET['q'];
+
 $db = new SQLite3('myfavoritedogs.db');
 
-// Example: Query data
-$result = $db->query('SELECT * FROM lists');
-while ($row = $result->fetchArray()) {
-    echo "ID: " . $row['id'] . ", Name: " . $row['name'] . "<br>";
-}
+$stmt1 = $db->prepare('SELECT photo_url FROM photos INNER JOIN dogbreeds ON dogbreeds.id = photos.breed_id WHERE dogbreeds.slug = :slug');
+$stmt1->bindValue(':slug', $slug, SQLITE3_TEXT);
+$result = $stmt1->execute();
+
+var_dump($result->fetchArray());
 
 $db->close();
